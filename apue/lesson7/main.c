135a05160f8345a74778e022a43106188479f667
Manual-S <1075866471@qq.com>
2022-02-17 07:03:10 -0800

    ADD:lesson7

---
+++ apue/lesson7/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+int main()
+{
+    int count = 0;
+    FILE *fp;
+    
+
+    while (1)
+    {
+        fp = fopen("tmp", "r");
+        if (fp == NULL)
+        {
+            // fprintf(stderr,"fopen() failed! errno = %d\n",errno);
+            // perror("fopen() error");  // 引号里的字段是自己规定的
+            printf("count = %d\n",count);
+            fprintf(stderr, "fopen error %s\n", strerror(errno)); // 第二种报错的方式
+            exit(1);
+        }
+        count++;
+    }
+
+    printf("count = %d\n",count);
+
+
+    exit(0);
+}
