f2238af6a6b960e1a93099f25eeac1a3340c0815
Manual-S <1075866471@qq.com>
2022-02-19 08:23:16 -0800

    ADD:server流量控制

--- miniproject/server/medialib.c
+++ miniproject/server/medialib.c
@@ -1,4 +1,15 @@
 #include "medialib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "medialib.h"
 
+struct channel_context_st
+{
+    chnid_t chnid;
+    char *desc;
+    glob_t mp3glob;
+    int pos;
+    int fd;
+    off_t offset;
+    mytbf_t *tbf;
+};
--- miniproject/server/mytbf.c
+++ miniproject/server/mytbf.c
@@ -16,6 +16,35 @@ struct mytbf_st
 
 static struct mytbf_st* job[MYTBF_MAX];
 static pthread_mutex_t mut_job = PTHREAD_MUTEX_INITIALIZER;
+static pthread_once_t init_once = PTHREAD_ONCE_INIT;
+pthread_t tid;
+
+static void* thr_alrm(void* p)
+{
+    int i = 0;
+    while(1)
+    {
+        pthread_mutex_lock(&mut_job));
+
+        for(i = 0;i < MYTBF_MAX;i++)
+        {
+            if(job[i] != NULL)
+            {
+                pthread_mutex_lock(&job[i]->mut);
+                job[i]->token = job[i]->token + job[i]->cps;
+                if(job[i]->token > job[i]->cps)
+                {
+                    job[i]->token = job[i]->burst;
+                }
+                pthread_cond_broadcast(&job[i]->cond);
+                pthread_mutex_unlock(&jon[i]->mut);
+            }
+        }
+
+        pthread_mutex_unlock(&mut_job);
+        sleep(1);
+    }
+}
 
 int get_free_pos_ublocked()
 {
@@ -31,10 +60,37 @@ int get_free_pos_ublocked()
     return -1;
 }
 
+static void mould_unload(void)
+{
+    pthread_cancel(tid);
+    pthread_join(tid);
+    int i;
+    for(i = 0;i < MYTBF_MAX;i++)
+    {
+        free(job[i]);
+    }
+}
+
+static void mould_load(void)
+{
+    pthread_t tid;
+    err = pthread_create(&tid,NULL,thr_alrm,NULL);
+    if(err)
+    {
+        perror("mould_load pthread_create error");
+        exit(1);
+    }
+
+    atexit(mould_unload);
+}
+
 mytbf_t* mytbf_init(int cps,int burst)
 {
     struct mytbf_st *me;
     int pos;
+
+    pthread_once();
+
     me = malloc(sizeof(*me));
     if (me == NULL)
     {
@@ -72,12 +128,24 @@ int mytbf_fetchtoken(mytbf_t* ptr,int size)
     }
     n = min(me->token,size);
     me->token = me->token - n;
-    pthread_mutex_unlock(&me->mut)
+    pthread_mutex_unlock(&me->mut);
+
+    return n;
 }
 //
-int mytbf_returntoken(mytbf_t *)
+int mytbf_returntoken(mytbf_t *ptr,int size)
 {
+    struct mytbf_st* me = ptr;
+    pthread_mutex_lock(&me->mut);
+    me->token = me->token + size;
+    if(me->token > me->burst)
+    {
+        me->token = me->burst;
+    }
+    pthread_cond_broadcast();  // 这里写在lock前面了
+    pthread_mutex_unlock(&me->mut);
 
+    return 0;
 }
 int mytbf_destory(mytbf_t *ptr)
 {
